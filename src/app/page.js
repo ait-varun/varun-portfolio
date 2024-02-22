052c0fd42a0b766da8770417c5e65b65703ccebf
varun <aitvarunbhatia@gmail.com>
2024-02-22 16:28:46 +0530

    added hero section

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,40 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-
+import profile from "./assets/images/profile-main.png";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <header>
+        <div className="container-fluid">
+          <nav className={styles.navbar}>
+            <div>
+              <span className={styles.logo}>Varun</span>
+            </div>
+            <div>
+              <button>Get in touch</button>
+            </div>
+          </nav>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      </header>
+      <section className={styles.hero}>
+        <div className="container">
+          <div className="row">
+            <div className={styles.heroImage}>
+              <Image src={profile} alt="Profile-pic" />
+              <div className={styles.heroContent}>
+                <h1>Hi, I am Varun</h1>
+                <h2>Web Developer</h2>
+                <p>
+                  jhorem rfpsum golor sidt amet, consectetur adipiscing elit,
+                  eiusmod tempor incididunt utcjhg labore bet dolore magna
+                  aliqua. Quis ipsum suspendisse ultrices gravida.
+                </p>
+                <button>Projects</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
