ef368b0bc2487b21a8ac9e12299ce4964f3cdd06
varun <aitvarunbhatia@gmail.com>
2024-03-01 17:19:18 +0530

    updated description

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,7 @@ import "bootstrap/dist/css/bootstrap.css";
 
 export const metadata = {
   title: "Varun Portfolio",
-  description: "",
+  description: "Portfolio of Varun till now. Just a guy who likes to code.",
 };
 
 export default function RootLayout({ children }) {
