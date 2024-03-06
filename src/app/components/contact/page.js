7b98d48a7173a23e847b9442aa33506a5d93fafd
varun <aitvarunbhatia@gmail.com>
2024-03-06 15:35:53 +0530

    updated contact form

--- src/app/components/contact/page.js
+++ src/app/components/contact/page.js
@@ -1,8 +1,9 @@
 "use client";
 import { useForm } from "react-hook-form";
 import { useState } from "react";
-import Link from "next/link";
-
+import styles from "./page.module.css";
+import Image from "next/image";
+import arrow from "../../assets/images/back-arrow-svgrepo-com.svg";
 export default function ContactForm() {
   const form = useForm();
   const {
@@ -24,66 +25,73 @@ export default function ContactForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="container">
-      <div className="d-flex align-items-center justify-content-between my-4">
+    <form onSubmit={handleSubmit(onSubmit)} className={styles.contactForm}>
+      <div
+        className={`${styles.getInTouch} d-flex align-items-center justify-content-between my-4`}>
         <h2>Get in Touch</h2>
-        <Link href="/">GO HOME</Link>
+        <Image src={arrow} alt="back-button" />
       </div>
       {showMessage && (
         <div className="alert alert-success mb-3" role="alert">
           I&apos;ll reach you soon!
         </div>
       )}
-      <div className="mb-3">
-        <label htmlFor="name" className="form-label">
-          Name
-        </label>
-        <input
-          type="text"
-          id="name"
-          className="form-control"
-          {...register("name", { required: "Name is required" })}
-        />
-        {errors.name && (
-          <div className="invalid-feedback">{errors.name.message}</div>
-        )}
+      <div className="container">
+        <div className="row">
+          <div className="mb-3 col-md-6">
+            <label htmlFor="name" className="form-label">
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              className="form-control"
+              {...register("name", { required: "Name is required" })}
+            />
+            {errors.name && (
+              <div className="invalid-feedback">{errors.name.message}</div>
+            )}
+          </div>
+          <div className="mb-3 col-md-6">
+            <label htmlFor="contact" className="form-label">
+              Contact
+            </label>
+            <input
+              type="text"
+              id="contact"
+              className="form-control"
+              {...register("contact", {
+                required: "Contact is required",
+                pattern: {
+                  value: /^\S+@\S+\.\S+$/,
+                  message: "Invalid email address",
+                },
+              })}
+            />
+            {errors.contact && (
+              <div className="invalid-feedback">{errors.contact.message}</div>
+            )}
+          </div>
+          <div className="mb-3">
+            <label htmlFor="address" className="form-label">
+              Location
+            </label>
+            <textarea
+              id="address"
+              className="form-control"
+              {...register("address", { required: "Address is required" })}
+            />
+            {errors.address && (
+              <div className="invalid-feedback">{errors.address.message}</div>
+            )}
+          </div>
+          <div className={styles.formBtn}>
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+        </div>
       </div>
-      <div className="mb-3">
-        <label htmlFor="contact" className="form-label">
-          Contact
-        </label>
-        <input
-          type="text"
-          id="contact"
-          className="form-control"
-          {...register("contact", {
-            required: "Contact is required",
-            pattern: {
-              value: /^\S+@\S+\.\S+$/,
-              message: "Invalid email address",
-            },
-          })}
-        />
-        {errors.contact && (
-          <div className="invalid-feedback">{errors.contact.message}</div>
-        )}
-      </div>
-      <div className="mb-3">
-        <label htmlFor="address" className="form-label">
-          Location
-        </label>
-        <textarea
-          id="address"
-          className="form-control"
-          {...register("address", { required: "Address is required" })}
-        />
-        {errors.address && (
-          <div className="invalid-feedback">{errors.address.message}</div>
-        )}
-      </div>
-      <button type="submit" className="btn btn-primary">
-        Submit
-      </button>
     </form>
   );
 }
