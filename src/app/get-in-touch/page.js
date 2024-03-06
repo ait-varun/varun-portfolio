a2188030505ab7683dc9a6b2ba6fa0a0ab70d49d
varun <aitvarunbhatia@gmail.com>
2024-03-06 15:16:39 +0530

    update get in touch

--- src/app/get-in-touch/page.js
+++ src/app/get-in-touch/page.js
@@ -3,6 +3,15 @@ import ContactForm from "../components/contact/page";
 export default function GetInTouch() {
   return (
     <div className={styles.getInTouch}>
+      <div className={styles.marqueeBox}>
+        <h2 className={styles.marqueeText}>
+          <span>&nbsp;GET IN TOUCH</span>
+          <span>&nbsp;GET IN TOUCH</span>
+          <span>&nbsp;GET IN TOUCH</span>
+          <span>&nbsp;GET IN TOUCH</span>
+          <span>&nbsp;GET IN TOUCH</span>
+        </h2>
+      </div>
       <ContactForm />
     </div>
   );
