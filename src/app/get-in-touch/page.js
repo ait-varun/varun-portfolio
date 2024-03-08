1032eb2e6bd8920ad0a0ef7c6c7b98c1423d6454
varun <aitvarunbhatia@gmail.com>
2024-03-08 12:41:52 +0530

    updated

--- src/app/get-in-touch/page.js
+++ src/app/get-in-touch/page.js
@@ -1,6 +1,5 @@
 import styles from "./page.module.css";
 import ContactForm from "../components/contact/page";
-import Footer from "../components/footer/page";
 export default function GetInTouch() {
   return (
     <div className={styles.getInTouch}>
