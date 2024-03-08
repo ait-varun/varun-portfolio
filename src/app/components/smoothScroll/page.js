e0ff9107181cf2764644057a8af3a8175e324108
varun <aitvarunbhatia@gmail.com>
2024-03-08 14:48:35 +0530

    updated

--- src/app/components/smoothScroll/page.js
+++ src/app/components/smoothScroll/page.js
@@ -6,12 +6,20 @@ export default function SmoothScroll({ children }) {
     <ReactLenis
       root
       options={{
+        duration: 1.2,
+        easing: (t) => (t === 1 ? 1 : 1 - Math.pow(2, -10 * t)),
+        direction: "vertical",
+        gestureDirection: "vertical",
         smooth: true,
-        autoRaf: true,
-        duration: 1.5,
-        smoothTouch: true,
+        smoothTouch: false,
+        touchMultiplier: 2,
       }}>
       {children}
     </ReactLenis>
   );
 }
+
+// smooth: true,
+// autoRaf: true,
+// duration: 1.5,
+// smoothTouch: true,
